eda3fe52dd3b532dce6d0ef163606c29baa9f4b5
DattPatel6 <dattpatel8249@gmail.com>
2024-12-26 21:49:05 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -4,24 +4,33 @@ public class Array {
          Scanner sc = new Scanner(System.in);
          System.out.print("enter size of array:");
          int size = sc.nextInt();
-         int num[] = new int[size];
+         int num[] = new int[size+1];
          
          //input
          for(int i=0; i<size; i++){
              System.out.print("enter element of array:" );
              num[i] = sc.nextInt();
-          
          }
-         //searching of elememt
-            System.out.print("enter element to be find:" );
-         int x = sc.nextInt();
-
          //output
          for(int i=0; i<num.length; i++){
+               System.out.println(num[i]);
+              }
+              
+               System.out.print("enter element to be find:" );
+                 int x = sc.nextInt();
+         
+        for(int i=0; i<num.length; i++){
              if(num[i] == x){
                   System.out.println("X found at imdex:" + i);
              }
-            
-         }
      }
+              System.out.println("for insert element");
+            System.out.print("enter array location:");
+                 int y = sc.nextInt();
+             System.out.print("enter new element:");
+                 int z = sc.nextInt();
+                
+
+           
+}
 }
